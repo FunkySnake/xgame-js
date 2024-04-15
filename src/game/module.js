67c2b2e8758ef4022ae1a9f29615ab0ff6f0d26b
Ilia Pchelarov <ilia.pchelarov.extern@allane.com>
2024-04-15 10:49:37 +0300

    A page with clean canvas added (hello world).

--- src/game/module.js
+++ src/game/module.js
@@ -3,7 +3,7 @@ const hello = (function () {
     var module = {};
     module.name = "World";
     module.greet = function () {
-        return "Hello " + this.name + "!";
+        return "Hello, " + this.name + "!";
     }
     return module;
 }());
